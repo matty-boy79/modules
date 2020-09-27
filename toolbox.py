22599c8303010ab35b2a77780541d0281699a244
Matt <mattyboy.watkins@gmail.com>
2020-09-27 22:20:47 +0100

    added if name main clause

--- toolbox.py
+++ toolbox.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 def generate_device_name(device, description):
     """ This function generates a name
     of a VNF running in RTP data center
@@ -5,8 +9,9 @@ def generate_device_name(device, description):
     datacenter = 'RTP'
     devices = {'firewall': 'Cisco_ASAv', 'router': 'Cisco_CSR-1000v'}
     device_type = devices[device]
-    name = f"{device_type}--{description}__{datacenter}"
+    name = "{}--{}__{}".format(device_type, description, datacenter)
     return name
+
 
 def is_ipv4_address(ip):
     """ Return True if ipv4 address,
@@ -24,3 +29,10 @@ def is_ipv4_address(ip):
         return False
 
     return True
+
+
+if __name__ == "__main__":
+    characters = string.ascii_lowercase
+    description = ''.join(random.choice(characters) for i in range(10))
+    device = ['router', 'firewall'][random.randrange(0, 2)]
+    print(generate_device_name(device, description))
